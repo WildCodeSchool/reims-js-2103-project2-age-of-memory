6ad1a852d7e0313cf47e0b9123821bdef83ae3ae
DThomas51 <thomas.dufour974@gmail.com>
2021-04-14 13:47:44 +0200

    Renamed files in english

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Regle from './Regle';
+import Rules from './Rules';
 import logo from './assets/logo.png';
 
 function Header() {
   return (
     <>
       <img className="logo" src={logo} alt="Age of Memory" />
-      <Regle />
+      <Rules />
     </>
   );
 }
