bf6c7057ee0fadff77d2ca00f55fcd1e22c9e665
DThomas51 <thomas.dufour974@gmail.com>
2021-04-14 11:28:58 +0200

    added logo in component's header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Regle from './components/Regle';
+import logo from './components/assets/logo.png';
 
 function Header() {
   return (
-    <Regle />
+    <>
+      <img className="logo" src={logo} alt="Age of Memory" />
+      <Regle />
+    </>
   );
 }
 
